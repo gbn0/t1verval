55eedf9988f67af5041dc1e782b107af69f56be7
Guilherme Kuhn <gvvkuhn@gmail.com>
2024-10-06 18:58:52 -0300

    Casos de teste feitos

--- trabalho1/src/main/java/com/trabalho1/CalculaTarifa.java
+++ trabalho1/src/main/java/com/trabalho1/CalculaTarifa.java
@@ -4,42 +4,41 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class CalculaTarifa {
-    
+
     public CalculaTarifa() {
 
     }
 
     public double calculaTarifa(LocalDateTime entrada, LocalDateTime saida, boolean vip) {
         // if(entrada == null || saida == null) {
-        //     System.out.println("Data de entrada ou saída não pode ser nula");
-        //     return 0;
+        // System.out.println("Data de entrada ou saída não pode ser nula");
+        // return 0;
         // } else if(entrada.isAfter(saida)) {
-        //     System.out.println("Data de entrada não pode ser depois da data de saída");
-        //     return 0;
+        // System.out.println("Data de entrada não pode ser depois da data de saída");
+        // return 0;
         // }
 
         Duration duracao = Duration.between(entrada, saida);
         double tarifa = 0;
         boolean pernoite = saida.isAfter(entrada.plusDays(1).withHour(8).withMinute(0).withSecond(0));
-        
+
         // 15 min de permanência
-        if(saida.isBefore(entrada.plusMinutes(15))) {
+        if (saida.isBefore(entrada.plusMinutes(15))) {
             return tarifa;
         }
 
-        // 1h ou menos de permanência
-        if(saida.isBefore(entrada.plusHours(1))) {
-            tarifa = 5.9;
-        }
-
         // acima de 1h sem pernoite
-        if(!pernoite) {
+        if (!pernoite) {
             tarifa = 5.9 + (duracao.toHours() - 1) * 2.5;
         }
 
         // pernoite
-        if(pernoite) {
+        if (pernoite) {
             tarifa = 50 * (saida.getDayOfMonth() - entrada.getDayOfMonth());
+        }
+        // 1h ou menos de permanência
+        if (saida.isBefore(entrada.plusHours(1))) {
+            tarifa = 5.9;
         }
 
         return vip ? tarifa * 0.5 : tarifa;
--- trabalho1/src/test/java/CalculaTarifaTests.java
+++ trabalho1/src/test/java/CalculaTarifaTests.java
@@ -7,18 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.trabalho1.CalculaTarifa;
 
-
 public class CalculaTarifaTests {
 
     private final CalculaTarifa calculaTarifa = new CalculaTarifa();
 
     @ParameterizedTest
     @CsvSource({
-        "2024-10-01T13:30:00, 2024-10-01T13:45:00, false, 0.0",
-        "2024-10-01T13:30:00, 2024-10-01T13:40:00, true, 0.0",
-        "2024-10-01T13:30:00, 2024-10-01T13:45:01, false, 5.9"
+            "2024-09-17T13:30:00, 2024-09-17T13:31:00, false, 0.0",
+            "2024-09-17T13:30:00, 2024-09-17T13:44:59, false, 0.0",
     })
-    void testaTarifaGratis(String entrada, String saida, boolean vip, double valorEsperado) {
+    void testaTarifaGratisNormal(String entrada, String saida, boolean vip, double valorEsperado) {
         LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
         LocalDateTime horarioSaida = LocalDateTime.parse(saida);
         assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
@@ -26,8 +24,8 @@ public class CalculaTarifaTests {
 
     @ParameterizedTest
     @CsvSource({
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, false, 5.9",
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, true, 2.95"
+            "2024-09-17T13:30:00, 2024-09-17T13:31:00, true, 0.0",
+            "2024-09-17T13:30:00, 2024-09-17T13:44:59, true, 0.0",
     })
     void testaTarifaGratisVip(String entrada, String saida, boolean vip, double valorEsperado) {
         LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
@@ -37,10 +35,11 @@ public class CalculaTarifaTests {
 
     @ParameterizedTest
     @CsvSource({
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, false, 5.9",
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, true, 2.95"
+            "2024-09-17T13:30:00, 2024-09-17T13:45:00, false, 5.9",
+            "2024-09-17T13:30:00, 2024-09-17T14:29:59, false, 5.9",
     })
-    void testaTarifaUmaHora(String entrada, String saida, boolean vip, double valorEsperado) {
+
+    void testaTarifaUmaHoraNormal(String entrada, String saida, boolean vip, double valorEsperado) {
         LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
         LocalDateTime horarioSaida = LocalDateTime.parse(saida);
         assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
@@ -48,23 +47,58 @@ public class CalculaTarifaTests {
 
     @ParameterizedTest
     @CsvSource({
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, false, 5.9",
-        "2024-09-17T13:30:00, 2024-09-17T14:30:00, true, 2.95"
-    })
-    void testaTarifaMeiaHora(String entrada, String saida, boolean vip, double valorEsperado) {
-        LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
-        LocalDateTime horarioSaida = LocalDateTime.parse(saida);
-        assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        
+            "2024-09-17T13:30:00, 2024-09-17T13:45:00, true, 2.95",
+            "2024-09-17T13:30:00, 2024-09-17T14:29:59, true, 2.95",
     })
     void testaTarifaUmaHoraVip(String entrada, String saida, boolean vip, double valorEsperado) {
         LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
         LocalDateTime horarioSaida = LocalDateTime.parse(saida);
         assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
     }
-    
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-09-17T13:30:00, 2024-09-17T15:30:00, false, 8.4",
+            "2024-09-17T13:30:00, 2024-09-17T16:29:59, false, 8.4",
+
+    })
+    void testaTarifaMaisUmaHoraSemPernoite(String entrada, String saida, boolean vip, double valorEsperado) {
+        LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
+        LocalDateTime horarioSaida = LocalDateTime.parse(saida);
+        assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-09-17T13:30:00, 2024-09-17T15:30:00, true, 4.2",
+            "2024-09-17T13:30:00, 2024-09-17T16:29:59, true, 4.2",
+
+    })
+    void testaTarifaMaisUmaHoraSemPernoiteVip(String entrada, String saida, boolean vip, double valorEsperado) {
+        LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
+        LocalDateTime horarioSaida = LocalDateTime.parse(saida);
+        assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-09-17T13:30:00, 2024-09-18T13:30:00, false, 50.0",
+            "2024-09-17T13:30:00, 2024-09-19T13:29:59, false, 100.0",
+    })
+    void testaTarifaPernoiteNormal(String entrada, String saida, boolean vip, double valorEsperado) {
+        LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
+        LocalDateTime horarioSaida = LocalDateTime.parse(saida);
+        assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-09-17T13:30:00, 2024-09-18T13:30:00, true, 25.0",
+            "2024-09-17T13:30:00, 2024-09-19T13:29:59, true, 50.0",
+    })
+    void testaTarifaPernoiteVip(String entrada, String saida, boolean vip, double valorEsperado) {
+        LocalDateTime horarioEntrada = LocalDateTime.parse(entrada);
+        LocalDateTime horarioSaida = LocalDateTime.parse(saida);
+        assertEquals(valorEsperado, calculaTarifa.calculaTarifa(horarioEntrada, horarioSaida, vip));
+    }
 }
